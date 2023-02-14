d2c33cfc77bd084289e1a73cb9fd18c0ac949b17
Calum Jarvis <92122419+caluhm@users.noreply.github.com>
2023-02-14 21:49:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+const buttonSubmit = document.querySelector('#submit');
+
+buttonSubmit.addEventListener('click', e => {
+    e.preventDefault();
+    let email = document.querySelector('#email');
+    if(!validEmail.test(email.value) || email.value == '') {
+        email.classList.toggle('email-action-container-invalid');
+        email.placeholder = 'Invalid email address...';
+        
+        setTimeout(() => {
+            email.classList.toggle('email-action-container-invalid');
+            email.placeholder = 'Your email address...';
+        }, 2000)
+    } else {
+        email.classList.toggle('email-action-container-valid');
+        email.value = '';
+        email.placeholder = 'Thank you for subscribing!';
+        
+        setTimeout(() => {
+            email.classList.toggle('email-action-container-valid');
+            email.placeholder = 'Your email address...';
+        }, 2000)
+    }
+});
